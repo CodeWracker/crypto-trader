6a3ccd08333f72f71b240228f57afa856821cac6
Rodrigo Ferraz Souza <rodrigo.souza289@etec.sp.gov.br>
2020-06-30 06:15:21 -0300

    corrigi o calculo do next op

--- src/index.ts
+++ src/index.ts
@@ -14,59 +14,73 @@ let nextOperation: OperationType = {
 };
 let operations: PlaceOrderResponseType[] = [];
 let myAccount: AccountInfoDataType;
-const acoountInfoCallback = (res: AccountInfoDataType) => {
-  myAccount = res;
-};
-const buyCallback = (res: PlaceOrderResponseType) => {
-  operations.push(res);
-  nextOperation = {
-    type: "sell",
-    value:
-      parseFloat(res.operations[0].price) +
-      (parseFloat(res.operations[0].price) *
-        parseFloat(res.operations[0].fee_rate)) /
-        100,
-  };
-};
-const sellCallback = (res: PlaceOrderResponseType) => {
-  nextOperation = {
-    type: "buy",
-    value:
-      parseFloat(res.operations[0].price) -
-      (parseFloat(res.operations[0].price) *
-        parseFloat(res.operations[0].fee_rate)) /
-        100,
-  };
-};
-const checkOperationValid = (type: string, value: number): boolean => {
-  return true;
-};
 const operationMethods = {
   async buy(ticker: TickerType) {
     console.log("\nBUY");
     await mercadoBTCNegotiationService.getAccountInfo(
-      acoountInfoCallback,
+      callbackMethods["accountInfo"],
       "accountInfo"
     );
     await mercadoBTCNegotiationService.placeOrder(
-      buyCallback,
+      callbackMethods[nextOperation.type],
       "BTCBRL",
       parseFloat(myAccount.balance.brl.available) / parseFloat(ticker.last),
       nextOperation.value ? nextOperation.value : parseFloat(ticker.last),
-      "sell"
+      nextOperation.type
     );
   },
   sell() {
     console.log("\nSELL");
     mercadoBTCNegotiationService.placeOrder(
-      sellCallback,
+      callbackMethods[nextOperation.type],
       "BRLBTC",
       parseFloat(myAccount.balance.btc.available),
       nextOperation.value,
-      "sell"
+      nextOperation.type
     );
   },
 };
+const callbackMethods = {
+  accountInfo(res: AccountInfoDataType) {
+    myAccount = res;
+  },
+  buy(res: PlaceOrderResponseType) {
+    operations.push(res);
+    nextOperation = {
+      type: "sell",
+      value:
+        parseFloat(res.operations[0].price) +
+        (parseFloat(res.operations[0].price) *
+          (parseFloat(res.operations[0].fee_rate) + 1)) /
+          100,
+    };
+  },
+  sell(res: PlaceOrderResponseType) {
+    nextOperation = {
+      type: "buy",
+      value:
+        parseFloat(res.operations[0].price) -
+        (parseFloat(res.operations[0].price) *
+          (parseFloat(res.operations[0].fee_rate) + 1)) /
+          100,
+    };
+  },
+};
+
+const checkOperationValid = (
+  type: string,
+  value: number,
+  last: number
+): boolean => {
+  let response: boolean;
+  !value
+    ? (response = true)
+    : type === "buy"
+    ? (response = last <= value)
+    : (response = last >= value);
+  return response;
+};
+
 const mainLoop = async () => {
   //console.log(operations);
 
@@ -75,12 +89,23 @@ const mainLoop = async () => {
     ticker = resp;
   }, "BTC");
   console.log(`last: ${ticker.last}`);
-  if (checkOperationValid(nextOperation.type, nextOperation.value)) {
+  console.log(`next op ${nextOperation.type}: ${nextOperation.value}`);
+
+  if (
+    checkOperationValid(
+      nextOperation.type,
+      nextOperation.value,
+      parseFloat(ticker.last)
+    )
+  ) {
     let op = operationMethods[nextOperation.type];
     op(ticker);
     console.log(`next operation type: ${nextOperation.type}`);
     console.log(`next operation value: ${nextOperation.value}`);
+  } else {
+    console.log("Ainda não ta no ponto");
   }
+
   await setTimeOutPromise(5000);
   mainLoop();
 };
